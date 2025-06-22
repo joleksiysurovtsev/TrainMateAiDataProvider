7ef14ce1aef1940703a5ceb45657e0d1dc247115
o.surovtsev <o.surovtsev@flolive.net>
2025-06-22 20:12:17 +0300

    update connectToPostgres

--- src/main/kotlin/db/Databases.kt
+++ src/main/kotlin/db/Databases.kt
@@ -7,6 +7,7 @@ import dev.surovtsev.service.MediaService
 import java.sql.Connection
 import java.sql.DriverManager
 import org.flywaydb.core.Flyway
+import java.net.URI
 
 fun Application.configureDatabases() {
     val dbConnection: Connection = connectToPostgres()
@@ -37,30 +38,21 @@ fun runFlywayMigration(url: String, user: String, password: String) {
     flyway.migrate()
 }
 
-/**
- * Makes a connection to a Postgres database.
- *
- * In order to connect to your running Postgres process,
- * please specify the following parameters in your configuration file:
- * - postgres.url -- Url of your running database process.
- * - postgres.user -- Username for database connection
- * - postgres.password -- Password for database connection
- *
- * If you don't have a database process running yet, you may need to [download]((https://www.postgresql.org/download/))
- * and install Postgres and follow the instructions [here](https://postgresapp.com/).
- * Then, you would be able to edit your url,  which is usually "jdbc:postgresql://host:port/database", as well as
- * user and password values.
- *
- * @return [Connection] that represent connection to the database. Please, don't forget to close this connection when
- * your application shuts down by calling [Connection.close]
- * */
-fun Application.connectToPostgres(): Connection {
-    Class.forName("org.postgresql.Driver")
 
-    val url = environment.config.property("postgres.url").getString()
-    log.info("Connecting to postgres database at $url")
-    val user = environment.config.property("postgres.user").getString()
-    val password = environment.config.property("postgres.password").getString()
+fun Application.connectToPostgres(): Connection {
+    val rawUrl = System.getenv("DATABASE_URL")
+        ?: error("DATABASE_URL is not set")
+
+    val uri = URI(rawUrl.replace("postgres://", "postgresql://"))
+
+    val userInfo = uri.userInfo?.split(":")
+        ?: error("Missing user credentials in DATABASE_URL")
+
+    val user = userInfo[0]
+    val password = userInfo.getOrNull(1) ?: ""
+    val url = "jdbc:postgresql://${uri.host}:${uri.port}${uri.path}"
+
+    log.info("Connecting to DB with jdbc URL: $url")
 
     return DriverManager.getConnection(url, user, password)
 }
