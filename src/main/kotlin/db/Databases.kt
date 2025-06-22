aff8cb12614edba26993e9ddd9eb49bf77d673a4
o.surovtsev <o.surovtsev@flolive.net>
2025-06-22 20:52:21 +0300

    update connectToPostgres

--- src/main/kotlin/db/Databases.kt
+++ src/main/kotlin/db/Databases.kt
@@ -4,6 +4,7 @@ import dev.surovtsev.api.configureExerciseRoutes
 import dev.surovtsev.service.ExerciseService
 import dev.surovtsev.service.MediaService
 import io.ktor.server.application.*
+import io.ktor.server.util.url
 import org.flywaydb.core.Flyway
 import java.net.URI
 import java.sql.Connection
@@ -35,13 +36,13 @@ fun Application.connectToPostgres(): Connection {
     val host = System.getenv("PGHOST") ?: "localhost"
     val port = System.getenv("PGPORT") ?: "5432"
     val db = System.getenv("PGDATABASE") ?: "railway"
-    val user = System.getenv("PGUSER") ?: "postgres"
-    val password = System.getenv("PGPASSWORD") ?: "password"
+    val user = System.getenv("POSTGRES_USER") ?: "postgres"
+    val password = System.getenv("POSTGRES_PASSWORD") ?: "password"
+    val defaultUrl = "jdbc:postgresql://$host:$port/$db"
+    val databaseUrl = System.getenv("DATABASE_URL") ?: defaultUrl
 
-    val url = "jdbc:postgresql://$host:$port/$db"
 
-    log.info("Connecting to DB: $url as user=$user")
 
     Class.forName("org.postgresql.Driver")
-    return DriverManager.getConnection(url, user, password)
+    return DriverManager.getConnection(databaseUrl, user, password)
 }
